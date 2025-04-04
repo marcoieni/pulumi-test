41f88c66de19fc0558fbc8f0a586b1cbb6e4c068
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2025-04-04 17:28:43 +0200

    add env var

--- __main__.py
+++ __main__.py
@@ -1,3 +1,9 @@
 import pulumi
+import pulumi_github as github
 
 print("Hello, Pulumi!")
+
+actions_variable_resource = github.ActionsVariable("actionsVariableResource",
+    repository="pulumi-test",
+    value="foo",
+    variable_name="bar")
