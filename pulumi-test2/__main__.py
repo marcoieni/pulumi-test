7c8ac19fa5cbf4877f4ba5e3accd77f9b0361278
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2025-04-09 17:04:04 +0200

    edit bar2 variable

--- pulumi-test2/__main__.py
+++ pulumi-test2/__main__.py
@@ -25,5 +25,5 @@ settings = service.DeploymentSettings("deployment_settings",
 
 actions_variable_resource = github.ActionsVariable("actionsVariableResource2",
     repository="pulumi-test",
-    value="foo22",
+    value="foo2222",
     variable_name="bar2")
