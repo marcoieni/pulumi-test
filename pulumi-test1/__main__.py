b3b4ba22fe7b194b3c5f8c86eff98a2b17181b24
Marco Ieni <11428655+marcoieni@users.noreply.github.com>
2025-04-09 17:37:41 +0200

    set secret (#10)

--- pulumi-test1/__main__.py
+++ pulumi-test1/__main__.py
@@ -7,3 +7,8 @@ actions_variable_resource = github.ActionsVariable("actionsVariableResource",
     repository="pulumi-test",
     value="foo5",
     variable_name="bar")
+
+actions_secret_resource = github.ActionsSecret("actionsSecretResource",
+    repository="pulumi-test",
+    secret_name="MY_SECRET",
+    plaintext_value="super-secret-value")
