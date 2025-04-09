2c8f01f91c7af6b09d594a0bb454256c1c6539ea
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2025-04-09 16:21:06 +0200

    create deployment

--- pulumi-test2/__main__.py
+++ pulumi-test2/__main__.py
@@ -1,7 +1,23 @@
 import pulumi
 import pulumi_github as github
+import pulumi_pulumiservice as service
 
 print("Hello, Pulumi!")
+
+config = pulumi.Config()
+
+settings = service.DeploymentSettings("deployment_settings",
+    organization="ieni",
+    project=pulumi.get_project(),
+    stack=pulumi.get_stack(),
+    source_context={
+        "git": {
+            "repo_url": "https://github.com/marcoieni/pulumi-test.git",
+            "branch": "refs/heads/main",
+            "repo_dir": "pulumi-test2"
+        }
+    }
+)
 
 actions_variable_resource = github.ActionsVariable("actionsVariableResource2",
     repository="pulumi-test",
