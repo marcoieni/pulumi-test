1120af0556fa47b2a596f6a99bd4e420bd4058eb
Marco Ieni <11428655+marcoieni@users.noreply.github.com>
2025-04-09 17:19:57 +0200

    pulumi-test1-var (#9)

--- pulumi-test1/__main__.py
+++ pulumi-test1/__main__.py
@@ -5,5 +5,5 @@ print("Hello, Pulumi!")
 
 actions_variable_resource = github.ActionsVariable("actionsVariableResource",
     repository="pulumi-test",
-    value="foo1",
+    value="foo5",
     variable_name="bar")
