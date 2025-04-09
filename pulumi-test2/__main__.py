a5e37a76ffd6d6efcbf9f8c5001b88e1e368fb91
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2025-04-09 15:49:09 +0200

    edit gh variables

--- pulumi-test2/__main__.py
+++ pulumi-test2/__main__.py
@@ -5,5 +5,5 @@ print("Hello, Pulumi!")
 
 actions_variable_resource = github.ActionsVariable("actionsVariableResource2",
     repository="pulumi-test",
-    value="foo2",
+    value="foo22",
     variable_name="bar2")
