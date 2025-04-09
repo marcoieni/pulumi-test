a9f44d5a274f991df9f91a012bd05c208e66f638
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2025-04-09 15:24:14 +0200

    fix

--- pulumi-test2/__main__.py
+++ pulumi-test2/__main__.py
@@ -6,4 +6,4 @@ print("Hello, Pulumi!")
 actions_variable_resource = github.ActionsVariable("actionsVariableResource2",
     repository="pulumi-test",
     value="foo2",
-    variable_name="bar")
+    variable_name="bar2")
