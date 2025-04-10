584037d9e6b583d1cd1b960b826d30f1bbb6f0d7
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2025-04-10 15:43:13 +0200

    extract codebuild.ts

--- ci-runners-ts/index.ts
+++ ci-runners-ts/index.ts
@@ -2,6 +2,7 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import { CodebuildProjectArgs, createCodebuildProject } from "./codebuild-project";
 import { createCodebuildRole } from "./iam";
+import { createCodebuildProjects } from "./codebuild";
 
 const config = new pulumi.Config();
 // The GitHub repository where the codebuild project will be used.
@@ -16,33 +17,10 @@ const githubConnection = new aws.codeconnections.Connection("github_connection",
 // Create the CodeBuild IAM role with the GitHub connection
 const codebuildRole = createCodebuildRole(githubConnection.arn);
 
-// Common parameters for all CodeBuild projects
 const commonParams = {
     serviceRole: codebuildRole.arn,
     repository: repository,
     codeConnectionArn: githubConnection.arn,
 };
 
-createCodebuildProject("ubuntu222c", {
-    name: "ubuntu-22-2c",
-    computeType: "BUILD_GENERAL1_SMALL",
-    ...commonParams,
-});
-
-createCodebuildProject("ubuntu224c", {
-    name: "ubuntu-22-4c",
-    computeType: "BUILD_GENERAL1_MEDIUM",
-    ...commonParams,
-});
-
-createCodebuildProject("ubuntu228c", {
-    name: "ubuntu-22-8c",
-    computeType: "BUILD_GENERAL1_LARGE",
-    ...commonParams,
-});
-
-createCodebuildProject("ubuntu2236c", {
-    name: "ubuntu-22-36c",
-    computeType: "BUILD_GENERAL1_XLARGE",
-    ...commonParams,
-});
+createCodebuildProjects(commonParams);
